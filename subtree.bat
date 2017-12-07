deedd5bcf8f90f1da7711303cb68f52d0b7c6d13
MIsike <domacles0124@gmail.com>
2017-12-08 00:58:15 +0800

    update subtree.bat

--- subtree.bat
+++ subtree.bat
@@ -1,8 +1,2 @@
-rem git remote add -f imgui https://github.com/ocornut/imgui.git
-rem git subtree add --prefix=imgui imgui master --squash
-
-rem git remote add -f GacUI https://github.com/vczh-libraries/GacUI.git
-rem git subtree add --prefix=GacUI GacUI master --squash
-
-git remote add -f GacUIRelease https://github.com/vczh-libraries/Release.git
-git subtree add --prefix=GacUIRelease GacUIRelease master --squash
+git remote add -f Release https://github.com/vczh-libraries/Release.git
+git subtree add --prefix=Release Release master --squash
---
+++ tmp.bat
@@ -0,0 +1,5 @@
+git remote add -f imgui https://github.com/ocornut/imgui.git
+git subtree add --prefix=imgui imgui master --squash
+
+git remote add -f GacUI https://github.com/vczh-libraries/GacUI.git
+git subtree add --prefix=GacUI GacUI master --squash
