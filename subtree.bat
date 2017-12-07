685c84b6f2025a1431f2a799a8874e07de0dc864
MIsike <domacles0124@gmail.com>
2017-12-08 01:04:20 +0800

    update bat

--- subtree.bat
+++
@@ -1,2 +0,0 @@
-git remote add -f Release https://github.com/vczh-libraries/Release.git
-git subtree add --prefix=Release Release master --squash
--- tmp.bat
+++ tmp.bat
@@ -3,3 +3,6 @@ git subtree add --prefix=imgui imgui master --squash
 
 git remote add -f GacUI https://github.com/vczh-libraries/GacUI.git
 git subtree add --prefix=GacUI GacUI master --squash
+
+git remote add -f Release https://github.com/vczh-libraries/Release.git
+git subtree add --prefix=Release Release master --squash
